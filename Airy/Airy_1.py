7dbf5a5b11ba6b679230acfd10f3d65459a1d5c8
Olaf Willocx <olaf@usa.com>
2022-04-01 23:29:41 +0200

    Airy_1

---
+++ Airy/Airy_1.py
@@ -0,0 +1,22 @@
+'''
+1D Airy pattern with vertical lines showing the zeros.
+'''
+
+import matplotlib.pyplot as plt
+from scipy.special import j1, jn_zeros
+from numpy import linspace, sin, pi
+import sys
+
+if __name__ == '__main__':
+    xl = float(sys.argv[1])
+    x = linspace(-xl, xl, int(xl * 10**3))
+    I = (2 * j1(x) / x) ** 2
+    x0 = jn_zeros(1, max(1, xl // pi)) # upper limit
+    plt.plot(x, I, 'k', [0, 0], [0, 1], 'k--')
+    for x0_ in x0:
+        if x0_ < xl:
+            plt.plot([ x0_,  x0_], [0, 1], 'k--',
+                     [-x0_, -x0_], [0, 1], 'k--')
+    plt.xlabel(r'$k a \sin \theta$')
+    plt.ylabel(r'$I$')
+    plt.show()
