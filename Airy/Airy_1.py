891dbd70c455828c34212272b5339369d90c3505
Olaf Willocx <olaf@usa.com>
2022-04-02 22:54:25 +0200

    _

--- Airy/Airy_1.py
+++ Airy/Airy_1.py
@@ -4,19 +4,22 @@
 
 import matplotlib.pyplot as plt
 from scipy.special import j1, jn_zeros
-from numpy import linspace, pi
-import sys
+from numpy import linspace, arange, pi
+from math import ceil
 
-if __name__ == '__main__':
-    xl = float(sys.argv[1])
-    x = linspace(-xl, xl, int(xl * 10**3))
-    I = (2 * j1(x) / x) ** 2
+ZEROS = True
+xl = 12
+res = 1000
+
+x = linspace(-xl, xl, res)
+I = (2 * j1(x) / x) ** 2
+plt.plot(x, I, 'k')
+if ZEROS:
     x0 = jn_zeros(1, max(1, xl // pi)) # upper limit
-    plt.plot(x, I, 'k', [0, 0], [0, 1], 'k--')
     for x0_ in x0:
         if x0_ < xl:
-            plt.plot([ x0_,  x0_], [0, 1], 'k--',
-                     [-x0_, -x0_], [0, 1], 'k--')
-    plt.xlabel(r'$k a \sin \theta$')
-    plt.ylabel(r'$I$')
-    plt.show()
+            plt.plot([-x0_, x0_], [0, 0], 'or')
+plt.xlim(-xl, xl)
+plt.xlabel(r'$k a \sin \theta$')
+plt.ylabel('$I / I_0$')
+plt.show()
